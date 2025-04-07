0aea8f6466d5f3ab682c5a4d136fc2062826e289
adlermore <armorbita@gmail.com>
2025-04-08 01:05:58 +0400

    Fix: Image Add/

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,25 @@
 import LoginPopup from '../(auth)/components/login/LoginPopup'
+import fixingLogo from '@/public/images/fixing_logo.png'
+import Image from 'next/image';
 
 function Login() {
   return (
-    <div className='login_page'><LoginPopup /></div>
+    <>
+      <div className='fixing_log mx-auto flex justify-center mb-[40px]'>
+        <Image
+          width={228}
+          height={51}
+          src={fixingLogo}
+          unoptimized={true}
+          alt="Fixing Logo"
+          priority={true}
+        />
+      </div>
+      <div className='login_page'>
+        <LoginPopup />
+      </div>
+    </>
+
   )
 }
 
